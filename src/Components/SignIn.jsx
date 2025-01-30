0626e9386dc3d4f17502c022ddb2d9fb3226a4a5
DLeonardoG <darkleonardog@gmail.com>
2025-01-30 13:18:19 -0500

    style: :lipstick: I've added some style to sign up
    I've added some style to sign up
    
    By @DLeonardoG

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -5,22 +5,27 @@ export const SignIn = () => {
 
     const navigate = useNavigate();
   return (
-    <div className='sign_background'>
-        <aside className='sign_aside'>
-            <div>
-                <h1>Ledrian</h1>
+    <div className='sign_background bg-gradient-to-r from-black via-gray-700 to-black bg-[length:200%_200%] h-screen flex justify-center items-center'>
+    <aside className='sign_aside w-1/2 flex justify-center items-center h-screen border border-black/10'>
+        <div>
+            <h1 className='text-white text-4xl'>Ledrian</h1>
+        </div>
+    </aside>
+    <main className='sign_container w-1/2 flex justify-around items-center h-screen flex-col bg-white'>
+        <h1 className='text-3xl mb-6'>Sign in</h1>
+        <form className='sign_form w-full flex flex-col items-center gap-5'>
+            <div className='flex'>
+                <input type="text" placeholder="name" className="border border-gray-300 p-2 rounded-lg w-4/5" />
+                <input type="text" placeholder="lastname" className="border border-gray-300 p-2 rounded-lg w-4/5" />
             </div>
-        </aside>
-        <main className='sign_container'>
-        <h1>Sign in</h1>
-            <form className='sign_form'>
-                <input type="text" placeholder="username" />
-                <input type="password" placeholder="password" />
-                <button>Login</button>
-            </form>
-            <p>Do you already have an account? <a href="" onClick={() => navigate("../login")}>Go login</a></p>
-        </main>
-
-    </div>
+            <input type="email" placeholder="email" className="border border-gray-300 p-2 rounded-lg w-4/5" />
+            <input type="text" placeholder="username" className="border border-gray-300 p-2 rounded-lg w-4/5" />
+            <input type="password" placeholder="password" className="border border-gray-300 p-2 rounded-lg w-4/5" />
+            <input type="password" placeholder="repeatPassword" className="border border-gray-300 p-2 rounded-lg w-4/5" />
+            <button className="bg-blue-500 text-white py-2 px-4 rounded-lg w-4/5">Login</button>
+        </form>
+        <p className="mt-4">Do you already have an account? <a href="#" className="text-blue-500 hover:underline" onClick={() => navigate("../login")}>Go login</a></p>
+    </main>
+</div>
   )
 }
