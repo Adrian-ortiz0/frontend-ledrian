5daaa70f740f83a92738259eacfd77cdbbbc9d08
Adrian-ortiz0 <dxniel7328@gmail.com>
2025-01-30 11:06:56 -0500

    feat: :sparkles: I'm currently working on settings sections

--- src/Components/SettingsComponents/SettingsFeed.jsx
+++ src/Components/SettingsComponents/SettingsFeed.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export const SettingsFeed = () => {
   return (
-    <div>SettingsFeed</div>
+    <div className='settings_feed-container'>
+        <h1>Settings</h1>
+        <div className='settings_feed-options'>
+            <button>Log out</button>
+            <button>User Terms</button>
+            <button id='deleteAccount'>Delete account</button>
+        </div>
+    </div>
   )
 }
