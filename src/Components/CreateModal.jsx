4671ee0e07e29cbac333673cdf795b1d967d8fa7
Adrian-ortiz0 <dxniel7328@gmail.com>
2025-01-30 11:32:05 -0500

    feat: :sparkles: Currently workin on menus

---
+++ src/Components/CreateModal.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CreateModal = () => {
+  return (
+    <div>CreateModal</div>
+  )
+}
--- src/Components/LedrianInterfazSubComponents/NavProfile.jsx
+++ src/Components/LedrianInterfazSubComponents/NavProfile.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import { Menu, MenuItem } from '@mui/material'
+import React, { useState } from 'react'
 
 export const NavProfile = () => {
+
+  const [anchorEl, setAnchorEl] = useState("")
+
+  const handleClose = () =>{
+    setAnchorEl(null)
+  }
+
+  <Menu id='profile-menu' anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+    <MenuItem>Profile</MenuItem>
+    <MenuItem>Log out</MenuItem>
+  </Menu>
+
   return (
     <div className='nav_profile-container'>
           <img src="/public/profile_icon.png" alt="" width={20} height={20} />
