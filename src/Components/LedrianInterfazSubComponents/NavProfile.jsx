5eb54bb8d06a8c9369c2b29a0646178e0154ae93
Adrian-ortiz0 <dxniel7328@gmail.com>
2025-01-30 11:46:16 -0500

    feat: :sparkles: I've added mini menu onto profile header section

--- src/Components/LedrianInterfazSubComponents/NavProfile.jsx
+++ src/Components/LedrianInterfazSubComponents/NavProfile.jsx
@@ -1,26 +1,42 @@
-import { Menu, MenuItem } from '@mui/material'
-import React, { useState } from 'react'
+import { IconButton, Menu, MenuItem } from '@mui/material';
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import React, { useState } from 'react';
+import { use } from 'react';
+import { useNavigate } from 'react-router';
 
 export const NavProfile = () => {
 
-  const [anchorEl, setAnchorEl] = useState("")
+  const navigate = useNavigate();
 
-  const handleClose = () =>{
-    setAnchorEl(null)
-  }
+  const [anchorEl, setAnchorEl] = useState(null);
 
-  <Menu id='profile-menu' anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
-    <MenuItem>Profile</MenuItem>
-    <MenuItem>Log out</MenuItem>
-  </Menu>
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
   return (
     <div className='nav_profile-container'>
-          <img src="/public/profile_icon.png" alt="" width={20} height={20} />
-          
-        <button>
+      <img src="/public/profile_icon.png" alt="profile icon" width={20} height={20}/>
+      <button>
         Adrian Ortiz
-        </button>
+        <IconButton onClick={handleClick} aria-controls='profile-menu' aria-haspopup='true'>
+          <MoreVertIcon />
+        </IconButton>
+      </button>
+      <Menu
+        id='profile-menu'
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={() => navigate("/profile")}>Profile</MenuItem>
+        <MenuItem onClick={() => navigate("/login")}>Log out</MenuItem>
+      </Menu>
     </div>
-  )
-}
+  );
+};
